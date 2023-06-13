86bb7f0520f83e846f2cd737788bd63cefad23bb
Rif77777 <130090788+Rif77777@users.noreply.github.com>
2023-06-13 22:40:30 +0200

    up

---
+++ index.js
@@ -0,0 +1,11 @@
+function isValidPassword(password) {
+    const SAVED_PASSWORD = 'jqueryismyjam';
+    // Change code below this line
+    const isMatch = a === b ;
+    
+  
+    // Change code above this line
+    return isMatch;
+  }
+  console.log('password' === "mangodab3st");
+  
